56c6ed0cf7f0d9fffd3b13143e80948df9b1e909
EastAtlantaSanta420 <vossp@countryday.net>
2017-03-16 09:24:46 -0400

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -12,3 +12,22 @@ public class AddressBook {
 		friends = new Contact[10];
 		numfriends = 0;
     }
+public void addContact (Contact c) {
+	friends[numFriends]= c;
+	numfriends++;
+
+public void printContacts  () {
+	for (int i=0; i<numfriends; i++)
+		friends[i].printContact90;
+}
+
+public int numContacts()
+
+		return numfriends;
+
+private int haveCOntact(String s) {
+	for (int i=0;i<numfriends;i++)
+	if (friends[i[.getName().equals(s)); {
+			return i;}
+	else {
+	return -1;
